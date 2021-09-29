9347ad32fdf06325036d186889fa8584bb0dcf9f
Bill <ceriustech@gmail.com>
2021-09-28 22:20:08 -0500

    started work on the script js.

--- webghtml/script.js
+++ webghtml/script.js
@@ -1,3 +1,13 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
+
+const form = document.querySelector('[data-form]');
+const queryParamsContainer = document.querySelector('[data-query-params]');
+const requestHeadersContainer = document.querySelector(
+	'[data-request-headers]'
+);
+const keyValueTemplate = document.querySelector('[data-key-value-template]');
+const responseHeadersContainer = document.querySelector(
+	'[data-response-headers]'
+);
