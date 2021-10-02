6d655e67f8a7ac2f03c294f72764abe88f0a2b28
Bill <ceriustech@gmail.com>
2021-10-02 11:08:38 -0500

    added the ability to add fields for query params and headers. Successful test illustrating ability to request json data with axios.

--- webghtml/script.js
+++ webghtml/script.js
@@ -11,3 +11,57 @@ const keyValueTemplate = document.querySelector('[data-key-value-template]');
 const responseHeadersContainer = document.querySelector(
 	'[data-response-headers]'
 );
+
+// Will add query params element on click
+document
+	.querySelector('[data-add-query-param-btn]')
+	.addEventListener('click', () => {
+		queryParamsContainer.append(createKeyValuePair());
+	});
+
+// Will add request headers element on click
+document
+	.querySelector('[data-add-request-header-btn]')
+	.addEventListener('click', () => {
+		requestHeadersContainer.append(createKeyValuePair());
+	});
+
+// Append a new key/value pair
+queryParamsContainer.append(createKeyValuePair());
+requestHeadersContainer.append(createKeyValuePair());
+
+function createKeyValuePair() {
+	const element = keyValueTemplate.content.cloneNode(true);
+
+	// Remove element on click
+	element.querySelector('[data-remove-btn').addEventListener('click', (e) => {
+		e.target.closest('[data-key-value-pair]').remove();
+	});
+
+	return element;
+}
+
+// for testing: set up an event listner for the form
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	axios({
+		url: document.querySelector('[data-url').value,
+		method: document.querySelector('[data-method').value,
+		params: keyValuePairsToObjects(queryParamsContainer),
+		headers: keyValuePairsToObjects(requestHeadersContainer),
+	}).then((response) => {
+		console.log(response);
+	});
+});
+
+// Converts key value pairs to objects
+function keyValuePairsToObjects(container) {
+	const pairs = container.querySelectorAll('[data-key-value-pair]');
+	return [...pairs].reduce((data, pair) => {
+		const key = pair.querySelector('[data-key]').value;
+		const value = pair.querySelector('[data-value]').value;
+
+		if (key === '') return data;
+		return { ...data, [key]: value };
+	}, {});
+}
